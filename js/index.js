982138468f40022c428de9c87ef4a2af98c07436
kenner-pacheco <fenix4454p@gmail.com>
2025-06-13 23:19:24 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,22 @@
+const inicio = document.getElementById("inicio");
+
+inicio.addEventListener("click", () => {
+
+  window.location.href = "index.html";
+    
+});
+
+const productos = document.getElementById("productos");
+
+productos.addEventListener("click", () => {
+
+  window.location.href = "productos.html";
+    
+});
+const nosotros = document.getElementById("nosotros");
+
+nosotros.addEventListener("click", () => {
+
+  window.location.href = "nosotros.html";
+    
+});
